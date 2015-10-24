cdde51cc4f0a0ced1b78694a54a28e1d96802b3d
kai <KaiAlexHiller@web.de>
2015-10-24 11:56:17 +0200

    Slick2D Hello World

---
+++ RoboTowers.java
@@ -0,0 +1,44 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class RoboTowers extends BasicGame
+{
+	public RoboTowers(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		//g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello RoboTowers");
+
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new RoboTowers("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(RoboTowers.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
