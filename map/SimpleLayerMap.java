0f35ae91035be3da1f782b9b52f488609a2f05f8
xTyrix <karsten.diekhoff@web.de>
2015-10-24 22:32:11 +0200

    wall algorythm done
    well exactly that + player spawn

--- map/SimpleLayerMap.java
+++ map/SimpleLayerMap.java
@@ -3,7 +3,7 @@ package map;
 public class SimpleLayerMap extends Map {
 
 	/*public static void main(String[] args) {
-		Map m = new SimpleLayerMap(200, 30);
+		Map m = new SimpleLayerMap(50, 50);
 		System.out.println(m);
 	}*/
 
@@ -13,6 +13,7 @@ public class SimpleLayerMap extends Map {
 
 	@Override
 	protected void fillTiles() {
+		// setup basic box
 		int midX = tiles.length/2;
 		int midY = tiles[0].length/2;
 		for (int i = 1; i < tiles.length-1; i++) {
@@ -27,11 +28,21 @@ public class SimpleLayerMap extends Map {
 		tiles[tiles.length-1][0] = TileType.Nothing;
 		tiles[0][tiles[0].length-1] = TileType.Nothing;
 		tiles[tiles.length-1][tiles[0].length-1] = TileType.Nothing;
+		
+		// setup random walls
+		double maxDist = Math.sqrt(Math.pow(midX, 2) + Math.pow(midY, 2)); // sqrt(midX^2 + midY^2) / maxDist
 		for (int x = 1; x < midX; x++) {
 			for (int y = 1; y < tiles[0].length-1; y++) {
-				int dist = Math.abs(midX-x)^2+Math.abs(midY-y)^2;
-				int threshold = (int) (Math.random() * Math.max(midX, midY) * 4); // still testing
-				if (dist < threshold) {
+				double relativeDist = Math.sqrt(Math.pow(Math.abs(midX-x), 2) + Math.pow(Math.abs(midY-y), 2)) / maxDist; // sqrt(|midX-x|^2 + |midY-y|^2) / maxDist
+				int wallCount = 0;
+				if (tiles[x-1][y] == TileType.Wall) {
+					wallCount++;
+				}
+				if (tiles[x][y-1] == TileType.Wall) {
+					wallCount++;
+				}
+				double threshold = Math.random();
+				if (Math.pow(relativeDist, 2)*0.5 < threshold - 0.075*wallCount) {
 					tiles[x][y] = TileType.Floor;
 					tiles[tiles.length-1-x][tiles[0].length-1-y] = TileType.Floor;
 				} else {
@@ -40,5 +51,16 @@ public class SimpleLayerMap extends Map {
 				}
 			}
 		}
+
+		// setup random player spawn
+		int playerX = (int) ((1 + Math.random())*0.125*tiles.length);
+		int playerY = (int) ((1 + Math.random())*0.125*tiles[0].length);
+		tiles[playerX][playerY] = TileType.PlayerSpawn;
+		tiles[tiles.length-1-playerX][tiles[0].length-1-playerY] = TileType.PlayerSpawn;
+		
+		// TODO:
+		// nothing Tiles
+		// tower slots
+		// powerups
 	}
 }
